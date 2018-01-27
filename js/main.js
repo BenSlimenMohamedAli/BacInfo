820ffa84ccdb63d8131f6c38f60116121d77a63a
dali15 <dslimen.15@gmail.com>
2018-01-27 21:17:23 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+    $(".tit").typed({
+        strings: ["مشروع الباكالوريا للسنة الدراسية 2016/2017", "تم إنجازه من قبل ", "نورهان الصولي و وفاء العبدلي و محمد الدريدي", "Eleve.tn : together towards a dream"],
+        typeSpeed: 75,
+        backDelay: 2500,
+        showCursor: true,
+    });
+});
